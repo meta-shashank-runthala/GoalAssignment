c6cb5aa7fc030b1063c083098c9402af5446beb7
Shashank Runthala <shashank.runthala@metacube.com>
2021-01-12 11:56:31 +0530

    toDoApp

---
+++ force-app/main/default/lwc/todoApp/todoApp.js
@@ -0,0 +1,32 @@
+import { LightningElement, track } from 'lwc';
+
+export default class TodoApp extends LightningElement {
+    toDoID = 3;
+    taskDetails = '';
+    
+    @track toDos = [
+        {id: 1, taskdetails: 'This is the first task added'},
+        {id: 2, taskdetails: 'This is the second task added'}
+    ];
+
+    handlechange(event){
+        this.taskDetails = event.target.value;
+    }
+    
+    addToDo(){
+        console.log(this.taskDetails);
+        this.toDoID += 1;
+        this.toDos = [
+            ...this.toDos,
+            {
+                id: this.toDoID,
+                taskdetails: this.taskDetails
+            }
+        ];
+        this.taskDetails = '';
+    }
+
+    removeToDo(event){
+        alert(event.target.value);
+    }
+}
