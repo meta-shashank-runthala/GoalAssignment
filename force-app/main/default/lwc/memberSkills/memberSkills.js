41e0ae7c2c4676ea31aae206dc09fe5345815c57
Shashank Runthala <shashank.runthala@metacube.com>
2021-01-13 14:33:53 +0530

    space modified

--- force-app/main/default/lwc/memberSkills/memberSkills.js
+++ force-app/main/default/lwc/memberSkills/memberSkills.js
@@ -8,69 +8,69 @@ import TEAM_FIELD from '@salesforce/schema/TeamMember__c.Team__c';
 import SKILLS_FIELD from '@salesforce/schema/TeamMember__c.Skills__c';
 
 export default class MemberSkills extends LightningElement {
-    name = '';
-    team = '';
-    skills = '';
-    @track teamOptions = [];
+	name = '';
+	team = '';
+	skills = '';
+	@track teamOptions = [];
 
-    handleNameChange(event) {
-        this.name = event.target.value;
-    }
+	handleNameChange(event) {
+		this.name = event.target.value;
+	}
 
-    handleTeamChange(event) {
-        this.team = event.target.value;
-    }
+	handleTeamChange(event) {
+		this.team = event.target.value;
+	}
 
-    handleSkillsChange(event) {
-        this.skills = event.target.value;
-    }
+	handleSkillsChange(event) {
+		this.skills = event.target.value;
+	}
 
-    @wire(getListOfTeams)
-    listOfTeams({error, data}){
-        if (data) {
-            for (let i = 0; i < data.length; i++) {
-                this.teamOptions = [...this.teamOptions, { value: data[i].Id, label: data[i].Name }];
-            }
-            this.error = undefined;
-        } else if (error) {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Error fetching teams',
-                    message: error.body.message,
-                    variant: 'error',
-                }),
-            );
-            this.teamOptions = [];
-        }
-    }
+	@wire(getListOfTeams)
+	listOfTeams({error, data}){
+		if (data) {
+			for (let i = 0; i < data.length; i++) {
+				this.teamOptions = [...this.teamOptions, { value: data[i].Id, label: data[i].Name }];
+			}
+			this.error = undefined;
+		} else if (error) {
+			this.dispatchEvent(
+				new ShowToastEvent({
+					title: 'Error fetching teams',
+					message: error.body.message,
+					variant: 'error',
+				}),
+			);
+			this.teamOptions = [];
+		}
+	}
 
-    createTeamMember(){
-        const fields = {};
-        fields[NAME_FIELD.fieldApiName] = this.name;
-        fields[TEAM_FIELD.fieldApiName] = this.team;
-        fields[SKILLS_FIELD.fieldApiName] = this.skills;
-        const recordInput = { apiName: TEAM_MEMBER_OBJECT.objectApiName, fields };
-        createRecord(recordInput)
-            .then(member => {
-                this.name = '';
-                this.team = '';
-                this.skills = '';
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Success',
-                        message: 'Member joined the team.',
-                        variant: 'success',
-                    }),
-                );
-            })
-            .catch(error => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Error creating member',
-                        message: error.body.message,
-                        variant: 'error',
-                    }),
-                );
-            });
-    }
+	createTeamMember(){
+		const fields = {};
+		fields[NAME_FIELD.fieldApiName] = this.name;
+		fields[TEAM_FIELD.fieldApiName] = this.team;
+		fields[SKILLS_FIELD.fieldApiName] = this.skills;
+		const recordInput = { apiName: TEAM_MEMBER_OBJECT.objectApiName, fields };
+		createRecord(recordInput)
+			.then(member => {
+				this.name = '';
+				this.team = '';
+				this.skills = '';
+				this.dispatchEvent(
+					new ShowToastEvent({
+						title: 'Success',
+						message: 'Member joined the team.',
+						variant: 'success',
+					}),
+				);
+			})
+			.catch(error => {
+				this.dispatchEvent(
+					new ShowToastEvent({
+						title: 'Error creating member',
+						message: error.body.message,
+						variant: 'error',
+					}),
+				);
+			});
+	}
 }
